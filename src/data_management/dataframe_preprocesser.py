25ec15f741c2c2d92befb8be6f1a3950b21b66a0
riccardopoiani <37558061+riccardopoiani@users.noreply.github.com>
2019-11-26 20:55:06 +0100

    Improve UCM preprocessing

--- src/data_management/dataframe_preprocesser.py
+++ src/data_management/dataframe_preprocesser.py
@@ -1,13 +1,9 @@
-from sklearn.cluster import KMeans
 import numpy as np
-from src.data_management.RecSys2019Reader import RecSys2019Reader
-from src.data_management.RecSys2019Reader_utils import get_ICM_numerical
-from src.data_management.data_reader import read_target_users, read_URM_cold_all
 import pandas as pd
 import scipy.sparse as sps
 from sklearn.preprocessing import Normalizer
 
-def get_preprocessed_dataframe(path="../data/", age_transformer = None, activity_transformer = None):
+def get_preprocessed_dataframe(path="../data/", age_transformer = None, activity_transformer = None, keep_warm_only=False):
     """
     Data preprocessing of URM and UCM.
 
@@ -48,11 +44,22 @@ def get_preprocessed_dataframe(path="../data/", age_transformer = None, activity
 
     # Adding user profile len information
     user_act = URM_all.sum(axis=1)
-    user_act = np.array(user_act).squeeze()
-    df_user_act = pd.DataFrame(user_act)
+    total_users = np.arange(df_URM['row'].max() + 1)
+    if keep_warm_only:
+        warm_users = df_URM['row'].unique()
+        warm_mask = np.in1d(total_users, warm_users)
+        user_act_warm_users = user_act[warm_mask]
+        df_user_act = pd.DataFrame(user_act_warm_users)
+        df_user = pd.DataFrame(warm_users)
+    else:
+        user_act = np.array(user_act).squeeze()
+        df_user_act = pd.DataFrame(user_act)
+        df_user = pd.DataFrame(total_users)
+    df_user.rename(columns={0: 'user'}, inplace=True)
     df_user_act.rename(columns={0: 'activity'},
                        inplace=True)
-    merged = pd.merge(UCM_age_region, df_user_act, right_index=True, left_on="user")
+    df_user_act = pd.merge(df_user_act, df_user, left_index=True, right_index=True)
+    merged = pd.merge(UCM_age_region, df_user_act, right_on="user", left_on="user")
 
     # Data pre-processing: One hot encoding region
     dfDummies = pd.get_dummies(merged[['user', 'region']]['region'], prefix='region')
@@ -63,9 +70,11 @@ def get_preprocessed_dataframe(path="../data/", age_transformer = None, activity
     merged = merged.drop_duplicates(subset="user",
                                     keep='first', inplace=False)
 
+    merged = merged.reset_index()
+
     # Data pre-processing: Normalizing age
     if age_transformer is None:
-        age_transformer = Normalizer
+        age_transformer = Normalizer()
     temp = merged['age'].values.tolist()
     temp = [temp]
     age_transformer.fit(temp)
@@ -73,7 +82,8 @@ def get_preprocessed_dataframe(path="../data/", age_transformer = None, activity
     age_norm = pd.DataFrame(res[0])
     age_norm.rename(columns={0: 'age_norm'},
                     inplace=True)
-    merged = pd.merge(merged, age_norm, right_index=True, left_on="user")
+    age_norm = pd.merge(age_norm, df_user, left_index=True, right_index=True)
+    merged = pd.merge(merged, age_norm, right_on="user", left_on="user")
     merged = merged[['user', 'activity', 'region_-1.0', 'region_0.0', 'region_2.0',
                      'region_3.0', 'region_4.0', 'region_5.0', 'region_6.0', 'region_7.0',
                      'age_norm']]
@@ -89,7 +99,8 @@ def get_preprocessed_dataframe(path="../data/", age_transformer = None, activity
     act_norm = pd.DataFrame(res[0])
     act_norm.rename(columns={0: 'act_norm'},
                     inplace=True)
-    merged = pd.merge(merged, act_norm, right_index=True, left_on="user")
+    act_norm = pd.merge(act_norm, df_user, left_index=True, right_index=True)
+    merged = pd.merge(merged, act_norm, right_on="user", left_on="user")
     merged.drop(columns=['activity'], inplace=True)
 
     # Re-setting index
