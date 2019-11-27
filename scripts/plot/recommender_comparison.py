239cee206f42cf6736ffe000a2391dc7e8945815
riccardopoiani <37558061+riccardopoiani@users.noreply.github.com>
2019-11-27 15:19:01 +0100

    Fix folder bugs + add comparison graph

--- scripts/plot/recommender_comparison.py
+++ scripts/plot/recommender_comparison.py
@@ -1,14 +1,9 @@
-from course_lib.Base.NonPersonalizedRecommender import TopPop
-from course_lib.GraphBased.P3alphaRecommender import P3alphaRecommender
-from course_lib.GraphBased.RP3betaRecommender import RP3betaRecommender
-from course_lib.MatrixFactorization.PureSVDRecommender import PureSVDRecommender
-from course_lib.KNN.ItemKNNCFRecommender import *
-from course_lib.KNN.UserKNNCFRecommender import *
-from course_lib.SLIM_BPR.Cython.SLIM_BPR_Cython import SLIM_BPR_Cython
 from src.data_management.New_DataSplitter_leave_k_out import *
 from src.data_management.RecSys2019Reader import RecSys2019Reader
 from src.data_management.RecSys2019Reader_utils import get_ICM_numerical
+from src.model.FallbackRecommender.AdvancedTopPopular import AdvancedTopPopular
 from src.plots.recommender_plots import *
+from src.data_management.dataframe_preprocesser import get_preprocessed_dataframe
 
 if __name__ == '__main__':
     # Data reading
@@ -18,11 +13,15 @@ if __name__ == '__main__':
     data_reader.load_data()
     URM_train, URM_test = data_reader.get_holdout_split()
 
+    mapper = data_reader.SPLIT_GLOBAL_MAPPER_DICT['user_original_ID_to_index']
+    df = get_preprocessed_dataframe("../../data/", keep_warm_only=True)
+
     # Build ICMs
     ICM_numerical, _ = get_ICM_numerical(data_reader.dataReader_object)
     ICM_categorical = data_reader.get_ICM_from_name("ICM_sub_class")
 
     # Building the recommenders
+    """
     item_cf_keywargs = {'topK': 5, 'shrink': 1000, 'similarity': 'cosine', 'normalize': True,
                         'feature_weighting': 'TF-IDF'}
     item_cf = ItemKNNCFRecommender(URM_train)
@@ -33,7 +32,6 @@ if __name__ == '__main__':
     user_cf = UserKNNCFRecommender(URM_train)
     user_cf.fit(**user_cf_keywargs)
 
-    """
     item_cbf_numerical_kwargs = {'feature_weighting': 'none', 'normalize': False, 'normalize_avg_row': True,
                        'shrink': 0, 'similarity': 'euclidean', 'similarity_from_distance_mode': 'exp',
                        'topK': 1000}
@@ -46,7 +44,7 @@ if __name__ == '__main__':
     item_cbf_categorical = ItemKNNCBFRecommender(ICM_categorical, URM_train)
     item_cbf_categorical.fit(**item_cbf_categorical_kwargs)
     item_cbf_categorical.RECOMMENDER_NAME = "ItemCBFKNNRecommenderCategorical"
-    """
+    
 
     p3alpha_kwargs = {'topK': 84, 'alpha': 0.6033770403001427, 'normalize_similarity': True}
     p3alpha = P3alphaRecommender(URM_train)
@@ -61,25 +59,33 @@ if __name__ == '__main__':
     slim_bpr = SLIM_BPR_Cython(URM_train)
     slim_bpr.fit(**slim_bpr_kwargs)
 
-    top_pop = TopPop(URM_train)
-    top_pop.fit()
-
     pure_svd_kwargs = {'num_factors': 350}
     pure_svd = PureSVDRecommender(URM_train)
     pure_svd.fit(**pure_svd_kwargs)
+    """
+
+    top_pop = TopPop(URM_train)
+    top_pop.fit()
+
+    advanced_top_pop_keywargs = advanced_keywargs =  {'clustering_method': 'kmodes', 'n_clusters': 7, 'init_method': 'Cao'}
+    advanced_top_pop = AdvancedTopPopular(URM_train, df, mapper)
+    advanced_top_pop.fit(**advanced_top_pop_keywargs)
+
 
 
     recommender_list = []
-    recommender_list.append(item_cf)
-    recommender_list.append(user_cf)
+    #recommender_list.append(item_cf)
+    #recommender_list.append(user_cf)
     recommender_list.append(top_pop)
+    recommender_list.append(advanced_top_pop)
     #recommender_list.append(item_cbf_numerical)
     #recommender_list.append(item_cbf_categorical)
-    recommender_list.append(p3alpha)
-    recommender_list.append(rp3beta)
-    recommender_list.append(pure_svd_kwargs)
-    recommender_list.append(slim_bpr)
+    #recommender_list.append(p3alpha)
+    #recommender_list.append(rp3beta)
+    #recommender_list.append(pure_svd_kwargs)
+    #recommender_list.append(slim_bpr)
+
 
     # Plotting the comparison based on user activity
-    plot_compare_recommender_user_group()
+    #plot_compare_recommender_user_group()
     plot_compare_recommenders_user_profile_len(recommender_list, URM_train, URM_test, save_on_file=True)
